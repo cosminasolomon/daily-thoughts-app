bf5055a06b752cfb5865ef233efb01922f3eac59
Cosmina Solomon <solomoncosminamaria@gmail.com>
2021-09-06 16:58:13 +0200

    entry model created

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from journal import views as post_views
+from rest_framework import routers, viewsets
+router = routers.DefaultRouter()
+router.register(r'post', post_views.PostView, 'webshop')
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
