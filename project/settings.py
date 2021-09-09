c1e81802fb453fca016702c1c93083e7790bac9c
Cosmina Solomon <solomoncosminamaria@gmail.com>
2021-09-09 11:33:04 +0200

    bruno

--- project/settings.py
+++ project/settings.py
@@ -146,3 +146,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'daily-thoughts-app/static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
